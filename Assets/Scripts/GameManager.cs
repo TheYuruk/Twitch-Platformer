3518a78ae19016d3b40cf82f7eb44ecec5ccd337
TheYuruk <yurukalikemal@gmail.com>
2021-01-19 18:14:29 +0300

    AnaProje

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] platformPrefabs;
+    
+    public int platformSpawnCount;
+    
+    public Vector3 lastEndPoint;
+    
+        
+        public void SpawnPlatforms()
+    
+    {  
+        //8 Times
+        for (int i = 0; i < platformSpawnCount; i++)
+        {
+            GameObject platform = GameObject.Instantiate(platformPrefabs[Random.Range(0,platformPrefabs.Length)]);
+            platform platformScript = platform.GetComponent<platform>();
+            platform.transform.position = lastEndPoint;
+            lastEndPoint = platformScript.ReturnEndPoint();
+        }
+    }
+    
+    void Start()
+    {
+        SpawnPlatforms();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
+ 
